e4834215d0188e8652e884498717f56e33a6147b
Bryce Simmons <bryces99@bigpond.com>
2022-01-10 04:08:28 +1030

    Removed console logging from previous commit

--- routes/index.js
+++ routes/index.js
@@ -361,7 +361,7 @@ router.get('/userinfo', function (req, res, next) {
             console.error(err);
             return;
         }
-        console.log(row);
+        // console.log(row);
         if ('minecraft_uuid' in row && row.minecraft_uuid !== null) {
             row.minecraft_skin = await getSkinFromUUID(row.minecraft_uuid);
         }
